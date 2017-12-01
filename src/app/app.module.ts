6ca73b50241d0c0726552eea5a003efe7f73972a
RANDRIAMIFIDY BEZAMA MAROLAHY <marolahy@gmail.com>
2017-12-01 09:08:56 +0300

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {AngularFireModule} from 'angularfire2';
+import { AngularFireDatabase,  } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 
